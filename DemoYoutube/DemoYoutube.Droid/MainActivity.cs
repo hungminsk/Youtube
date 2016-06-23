0beed3068e21d9cd038c7ec7fa8048dd1425eafa
hungminsk <hungminsk@hotmail.com>
2016-06-23 11:42:17 +0700

    Add material design

--- DemoYoutube/DemoYoutube.Droid/MainActivity.cs
+++ DemoYoutube/DemoYoutube.Droid/MainActivity.cs
@@ -6,17 +6,21 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
 
 namespace DemoYoutube.Droid
 {
     [Activity(Label = "DemoYoutube", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            FormsAppCompatActivity.TabLayoutResource = Resource.Layout.tabs;
+
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Forms.Init(this, bundle);
             LoadApplication(new App());
         }
     }
---
+++ DemoYoutube/DemoYoutube/Views/VideoDetails.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DemoYoutube.Views
+{
+    public partial class VideoDetails : ContentPage
+    {
+        public VideoDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
