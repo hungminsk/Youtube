a0b06551b1a608697fbf2e54aebd7a18bbd9e427
hungminsk <hungminsk@hotmail.com>
2016-06-28 11:44:36 +0700

    Add ios binding

--- YoutubePlayer_iOS/ApiDefinition.cs
+++ YoutubePlayer_iOS/ApiDefinition.cs
@@ -1,152 +1,148 @@
-﻿using System;
+using System;
 
-using ObjCRuntime;
 using Foundation;
+using ObjCRuntime;
 using UIKit;
 
-namespace YoutubePlayer_iOS
-{
-    [Model, BaseType(typeof(NSObject))]
-    [Protocol]
-    public partial interface YTPlayerViewDelegate
-    {
+namespace YTHelper {
 
-        [Export("playerViewDidBecomeReady:")]
-        void DidBecomeReady(YTPlayerView playerView);
+	[Model, BaseType (typeof (NSObject))][Protocol]
+	public partial interface YTPlayerViewDelegate {
 
-        [Export("playerView:didChangeToState:")]
-        void DidChangeToState(YTPlayerView playerView, YTPlayerState state);
+		[Export ("playerViewDidBecomeReady:")]
+		void DidBecomeReady (YTPlayerView playerView);
 
-        [Export("playerView:didChangeToQuality:")]
-        void DidChangeToQuality(YTPlayerView playerView, YTPlaybackQuality quality);
+		[Export ("playerView:didChangeToState:")]
+		void DidChangeToState (YTPlayerView playerView, YTPlayerState state);
 
-        [Export("playerView:receivedError:")]
-        void ReceivedError(YTPlayerView playerView, YTPlayerError error);
-    }
+		[Export ("playerView:didChangeToQuality:")]
+		void DidChangeToQuality (YTPlayerView playerView, YTPlaybackQuality quality);
 
-    [BaseType(typeof(UIView))]
-    public partial interface YTPlayerView : IUIWebViewDelegate
-    {
+		[Export ("playerView:receivedError:")]
+		void ReceivedError (YTPlayerView playerView, YTPlayerError error);
+	}
 
-        [Export("webView", ArgumentSemantic.Retain)]
-        UIWebView WebView { get; }
+	[BaseType (typeof (UIView))]
+	public partial interface YTPlayerView : IUIWebViewDelegate {
 
-        [Export("delegate", ArgumentSemantic.Assign)]
-        YTPlayerViewDelegate Delegate { get; set; }
+		[Export ("webView", ArgumentSemantic.Retain)]
+		UIWebView WebView { get; }
 
-        [Export("loadWithVideoId:")]
-        bool LoadWithVideoId(string videoId);
+		[Export ("delegate", ArgumentSemantic.Assign)]
+		YTPlayerViewDelegate Delegate { get; set; }
 
-        [Export("loadWithPlaylistId:")]
-        bool LoadWithPlaylistId(string playlistId);
+		[Export ("loadWithVideoId:")]
+		bool LoadWithVideoId (string videoId);
 
-        [Export("loadWithVideoId:playerVars:")]
-        bool LoadWithVideoId(string videoId, NSDictionary playerVars);
+		[Export ("loadWithPlaylistId:")]
+		bool LoadWithPlaylistId (string playlistId);
 
-        [Export("loadWithPlaylistId:playerVars:")]
-        bool LoadWithPlaylistId(string playlistId, NSDictionary playerVars);
+		[Export ("loadWithVideoId:playerVars:")]
+		bool LoadWithVideoId (string videoId, NSDictionary playerVars);
 
-        [Export("playVideo")]
-        void PlayVideo();
+		[Export ("loadWithPlaylistId:playerVars:")]
+		bool LoadWithPlaylistId (string playlistId, NSDictionary playerVars);
 
-        [Export("pauseVideo")]
-        void PauseVideo();
+		[Export ("playVideo")]
+		void PlayVideo ();
 
-        [Export("stopVideo")]
-        void StopVideo();
+		[Export ("pauseVideo")]
+		void PauseVideo ();
 
-        [Export("seekToSeconds:allowSeekAhead:")]
-        void SeekToSeconds(float seekToSeconds, bool allowSeekAhead);
+		[Export ("stopVideo")]
+		void StopVideo ();
 
-        [Export("clearVideo")]
-        void ClearVideo();
+		[Export ("seekToSeconds:allowSeekAhead:")]
+		void SeekToSeconds (float seekToSeconds, bool allowSeekAhead);
 
-        [Export("cueVideoById:startSeconds:suggestedQuality:")]
-        void CueVideoById(string videoId, float startSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("clearVideo")]
+		void ClearVideo ();
 
-        [Export("cueVideoById:startSeconds:endSeconds:suggestedQuality:")]
-        void CueVideoById(string videoId, float startSeconds, float endSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("cueVideoById:startSeconds:suggestedQuality:")]
+		void CueVideoById (string videoId, float startSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("loadVideoById:startSeconds:suggestedQuality:")]
-        void LoadVideoById(string videoId, float startSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("cueVideoById:startSeconds:endSeconds:suggestedQuality:")]
+		void CueVideoById (string videoId, float startSeconds, float endSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("loadVideoById:startSeconds:endSeconds:suggestedQuality:")]
-        void LoadVideoById(string videoId, float startSeconds, float endSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("loadVideoById:startSeconds:suggestedQuality:")]
+		void LoadVideoById (string videoId, float startSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("cueVideoByURL:startSeconds:suggestedQuality:")]
-        void CueVideoByURL(string videoURL, float startSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("loadVideoById:startSeconds:endSeconds:suggestedQuality:")]
+		void LoadVideoById (string videoId, float startSeconds, float endSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("cueVideoByURL:startSeconds:endSeconds:suggestedQuality:")]
-        void CueVideoByURL(string videoURL, float startSeconds, float endSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("cueVideoByURL:startSeconds:suggestedQuality:")]
+		void CueVideoByURL (string videoURL, float startSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("loadVideoByURL:startSeconds:suggestedQuality:")]
-        void LoadVideoByURL(string videoURL, float startSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("cueVideoByURL:startSeconds:endSeconds:suggestedQuality:")]
+		void CueVideoByURL (string videoURL, float startSeconds, float endSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("loadVideoByURL:startSeconds:endSeconds:suggestedQuality:")]
-        void LoadVideoByURL(string videoURL, float startSeconds, float endSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("loadVideoByURL:startSeconds:suggestedQuality:")]
+		void LoadVideoByURL (string videoURL, float startSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("cuePlaylistByPlaylistId:index:startSeconds:suggestedQuality:")]
-        void CuePlaylistByPlaylistId(string playlistId, int index, float startSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("loadVideoByURL:startSeconds:endSeconds:suggestedQuality:")]
+		void LoadVideoByURL (string videoURL, float startSeconds, float endSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("cuePlaylistByVideos:index:startSeconds:suggestedQuality:")]
-        void CuePlaylistByVideos(NSObject[] videoIds, int index, float startSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("cuePlaylistByPlaylistId:index:startSeconds:suggestedQuality:")]
+		void CuePlaylistByPlaylistId (string playlistId, int index, float startSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("loadPlaylistByPlaylistId:index:startSeconds:suggestedQuality:")]
-        void LoadPlaylistByPlaylistId(string playlistId, int index, float startSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("cuePlaylistByVideos:index:startSeconds:suggestedQuality:")]
+		void CuePlaylistByVideos (NSObject [] videoIds, int index, float startSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("loadPlaylistByVideos:index:startSeconds:suggestedQuality:")]
-        void LoadPlaylistByVideos(NSObject[] videoIds, int index, float startSeconds, YTPlaybackQuality suggestedQuality);
+		[Export ("loadPlaylistByPlaylistId:index:startSeconds:suggestedQuality:")]
+		void LoadPlaylistByPlaylistId (string playlistId, int index, float startSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("nextVideo")]
-        void NextVideo();
+		[Export ("loadPlaylistByVideos:index:startSeconds:suggestedQuality:")]
+		void LoadPlaylistByVideos (NSObject [] videoIds, int index, float startSeconds, YTPlaybackQuality suggestedQuality);
 
-        [Export("previousVideo")]
-        void PreviousVideo();
+		[Export ("nextVideo")]
+		void NextVideo ();
 
-        [Export("playVideoAt:")]
-        void PlayVideoAt(int index);
+		[Export ("previousVideo")]
+		void PreviousVideo ();
 
-        [Export("playbackRate")]
-        float PlaybackRate { get; set; }
+		[Export ("playVideoAt:")]
+		void PlayVideoAt (int index);
 
-        [Export("availablePlaybackRates")]
-        NSObject[] AvailablePlaybackRates { get; }
+		[Export ("playbackRate")]
+		float PlaybackRate { get; set; }
 
-        [Export("loop")]
-        bool Loop { set; }
+		[Export ("availablePlaybackRates")]
+		NSObject [] AvailablePlaybackRates { get; }
 
-        [Export("shuffle")]
-        bool Shuffle { set; }
+		[Export ("loop")]
+		bool Loop { set; }
 
-        [Export("videoLoadedFraction")]
-        float VideoLoadedFraction { get; }
+		[Export ("shuffle")]
+		bool Shuffle { set; }
 
-        [Export("playerState")]
-        YTPlayerState PlayerState { get; }
+		[Export ("videoLoadedFraction")]
+		float VideoLoadedFraction { get; }
 
-        [Export("currentTime")]
-        float CurrentTime { get; }
+		[Export ("playerState")]
+		YTPlayerState PlayerState { get; }
 
-        [Export("playbackQuality")]
-        YTPlaybackQuality PlaybackQuality { get; set; }
+		[Export ("currentTime")]
+		float CurrentTime { get; }
 
-        [Export("availableQualityLevels")]
-        NSObject[] AvailableQualityLevels { get; }
+		[Export ("playbackQuality")]
+		YTPlaybackQuality PlaybackQuality { get; set; }
 
-        [Export("duration")]
-        int Duration { get; }
+		[Export ("availableQualityLevels")]
+		NSObject [] AvailableQualityLevels { get; }
 
-        [Export("videoUrl")]
-        NSUrl VideoUrl { get; }
+		[Export ("duration")]
+		int Duration { get; }
 
-        [Export("videoEmbedCode")]
-        string VideoEmbedCode { get; }
+		[Export ("videoUrl")]
+		NSUrl VideoUrl { get; }
 
-        [Export("playlist")]
-        NSObject[] Playlist { get; }
+		[Export ("videoEmbedCode")]
+		string VideoEmbedCode { get; }
 
-        [Export("playlistIndex")]
-        int PlaylistIndex { get; }
-    }
+		[Export ("playlist")]
+		NSObject [] Playlist { get; }
+
+		[Export ("playlistIndex")]
+		int PlaylistIndex { get; }
+	}
 }
-
---
+++ YoutubePlayer_iOS/StructsAndEnums.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace YTHelper
+{
+	public enum YTPlayerState {
+		Unstarted,
+		Ended,
+		Playing,
+		Paused,
+		Buffering,
+		Queued,
+		Unknown
+	}
+
+	public enum YTPlaybackQuality {
+		Small,
+		Medium,
+		Large,
+		HD720,
+		HD1080,
+		HighRes,
+		Unknown
+	}
+
+	public enum YTPlayerError {
+		InvalidParam,
+		HTML5Error,
+		VideoNotFound,
+		NotEmbeddable,
+		Unknown
+	}
+}
